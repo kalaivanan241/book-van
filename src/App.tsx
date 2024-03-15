fb2554aa49ba0f9deaa9b8ef5900236d67b4e6c4
kalai241 <kalaivanan241@gmail.com>
2024-03-15 15:53:15 +0800

    updated readme

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ function App() {
           <div>
             <button
               type="submit"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center "
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center disabled:bg-blue-200"
               disabled={isLoading}
             >
               {isLoading ? "Loading..." : "Submit"}
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   readonly VITE_GOOGLE_MAP_API_KEY: string;
-  // more env variables...
 }
 
 interface ImportMeta {
